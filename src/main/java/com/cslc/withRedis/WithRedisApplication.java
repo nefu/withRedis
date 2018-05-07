9c46cb3e3fba08f3c69a7dfe3f974d3d690f2173
chenjay <1101143623@qq.com>
2018-05-07 10:20:40 +0800

    新增了定时任务的注解

--- src/main/java/com/cslc/withRedis/WithRedisApplication.java
+++ src/main/java/com/cslc/withRedis/WithRedisApplication.java
@@ -2,7 +2,9 @@ package com.cslc.withRedis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+//启动定时任务配置
+@EnableScheduling
 @SpringBootApplication
 public class WithRedisApplication {
 
